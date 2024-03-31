84a0fdf4386f8f6fc87bb96018693f3c9f7c7f83
sujitashresthya <sujitashrestha1225@gmail.com>
2024-03-31 07:19:17 +0545

    first commit

--- expensesharing/settings.py
+++ expensesharing/settings.py
@@ -1,4 +1,4 @@
-from decouple import config
+# from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = ("dfguysdgfiug")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
